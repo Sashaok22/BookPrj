c86829c9200b017681db2e98265ac8693edeef73
PonyHammer PonyHammer <SASHAOK061999@GMAIL.COM>
2022-12-07 22:58:39 +0400

    Creating documentation

--- BookPrj/models/Authors.py
+++ BookPrj/models/Authors.py
@@ -35,15 +35,15 @@ class Authors(Base):
                 'date_of_death': self.date_of_death}
 
 
-class AuthorsSchema(BaseModel):
+class AuthorSchema(BaseModel):
     id: Optional[int] = Field(description="Author person id field")
     author_name: str = Field(..., min_length=2, description="Author name field")
     author_surname: str = Field(..., min_length=2, description="Author surname field")
     author_patronymic: Optional[str] = Field(min_length=2, description="Author patronymic field")
     date_of_birth: date = Field(..., description="Author date of birth field")
     date_of_death: Optional[date] = Field(description="Author date of death field")
-    genres: Optional[list[GenresSchema]] = Field(..., description="List of genres in which the author writes field")
-    books: Optional[list[BooksSchema]] = Field(..., description="List of books by this author field")
+    genres: Optional[list[GenresSchema]] = Field(description="List of genres in which the author writes field")
+    books: Optional[list[BooksSchema]] = Field(description="List of books by this author field")
 
     class Config:
         orm_mode = True
@@ -52,3 +52,7 @@ class AuthorsSchema(BaseModel):
 class WebError(BaseModel):
     error_code: int
     msg: str
+
+
+class AuthorsSchema(BaseModel):
+    Authors: Optional[list[AuthorSchema]]
