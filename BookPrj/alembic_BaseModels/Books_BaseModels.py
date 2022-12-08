44c573d121a0a8eb33d58eb255ec54abe163417d
PonyHammer PonyHammer <SASHAOK061999@GMAIL.COM>
2022-12-08 23:14:18 +0400

    finalisation.

--- BookPrj/alembic_BaseModels/Books_BaseModels.py
+++ BookPrj/alembic_BaseModels/Books_BaseModels.py
@@ -1,12 +1,30 @@
 from typing import Optional
 from pydantic import BaseModel, Field
+from alembic_BaseModels.Authors_BaseModels import AuthorSchema
+from alembic_BaseModels.Genres_BaseModels import GenresSchema
 
 
-class BooksSchema(BaseModel):
+class BookSchema(BaseModel):
     _id: Optional[int]
-    book_name: str = Field(..., min_length=2)
-    number_of_pages: int = Field(..., gt=0)
-    rating: int = Field(..., gt=0, lt=11)
+    book_name: str = Field(..., min_length=2, description='Book name field')
+    number_of_pages: int = Field(..., gt=0, description='Number of pages field')
+    rating: int = Field(..., gt=0, lt=11, description='Personal rating of books fields')
 
     class Config:
         orm_mode = True
+
+
+class Book_content(BookSchema):
+    genres: Optional[list[GenresSchema]] \
+        = Field(description="Genre id of the book, returned as a comma-separated string")
+    authors: Optional[list[AuthorSchema]] \
+        = Field(description="Author id of the book, returned as a comma-separated string")
+
+
+class Book_content_id(BookSchema):
+    genres_id: list[int] = Field(..., description="Book genre id")
+    authors_id: list[int] = Field(..., description="Book author id")
+
+
+class BooksSchema(BaseModel):
+    books: Optional[list[Book_content]] = Field(description="List of models of books")
--- BookPrj/routes/books_routes.py
+++ BookPrj/routes/books_routes.py
@@ -1,6 +1,11 @@
-from flask import request, jsonify, abort, Blueprint
+from flask import request, abort, Blueprint, make_response
 from pydantic import ValidationError
-from alembic_BaseModels.Books_BaseModels import BooksSchema
+from spectree import Response
+from SpecTree_config import spec
+from alembic_BaseModels.Authors_BaseModels import AuthorSchema
+from alembic_BaseModels.Books_BaseModels import BooksSchema, Book_content, BookSchema, Book_content_id
+from alembic_BaseModels.Genres_BaseModels import GenresSchema
+from alembic_BaseModels.Others_BaseModels import WebError
 from models.Authors import Authors
 from models.Books import Books
 from models.Genres import Genres
@@ -10,103 +15,161 @@ books_blueprint = Blueprint(__name__.split(".")[-1], __name__)
 
 
 @books_blueprint.get("/api/books")
+@spec.validate(resp=Response(HTTP_404=(WebError, "Books not found"),
+                             HTTP_200=(BooksSchema, 'Successful operation')), tags=["Books_request"])
 def get_books(db: Session = Session()):
+    """
+        Get all books
+
+        Return all books with all their authors and genres
+    """
     book = db.query(Books)
     if not request.values:
         _book = book.all()
     else:
         reit = request.values['rating']
         _book = book.filter(Books.rating == reit).all()
-    _book = [
-        {
-            "book": b,
-            "authors": b.Books_Authors,
-            "genres": b.Books_Genres
-        }
-        for b in _book
-    ]
     if not _book:
-        abort(404)
-    return jsonify(_book)
+        abort(make_response(WebError(error_code=404, msg="Books not found").dict(), 404))
+    all_books = []
+    for b in _book:
+        book = Book_content.from_orm(b)
+        genres = b.Books_Genres
+        book.genres = [GenresSchema.from_orm(g) for g in genres]
+        authors = b.Books_Authors
+        book.books = [AuthorSchema.from_orm(a) for a in authors]
+        all_books.append(book)
+    response = BooksSchema()
+    response.books = all_books
+    return response
 
 
 @books_blueprint.get("/api/books/<int:book_id>")
+@spec.validate(resp=Response(HTTP_404=(WebError, "Books not found"),
+                             HTTP_200=(Book_content, 'Successful operation')), tags=["Books_request"])
 def get_book(book_id, db: Session = Session()):
-    book = db.query(Books).filter(Books.id == book_id).all()
+    """
+        Find one book by id
+
+        Return one book by id with all its authors and genres
+    """
+    book = db.query(Books).get(book_id)
     if book is None:
-        abort(404)
-    book = [
-        {
-            "book": book[0],
-            "authors": book[0].Books_Authors,
-            "genres": book[0].Books_Genres
-        }
-    ]
-    return jsonify(book)
+        abort(make_response(WebError(error_code=404, msg="Books not found").dict(), 404))
+    _book = Book_content.from_orm(book)
+    genres = book.Books_Genres
+    _book.genres = [GenresSchema.from_orm(g) for g in genres]
+    authors = book.Books_Authors
+    _book.books = [AuthorSchema.from_orm(a) for a in authors]
+    return _book
 
 
 @books_blueprint.post("/api/books")
+@spec.validate(json=Book_content_id,
+               resp=Response(HTTP_400=(WebError, "Request data error"),
+                             HTTP_404=(WebError, "Books not found"),
+                             HTTP_200=(Book_content, 'Successful operation')), tags=["Books_request"])
 def create_book(db: Session = Session()):
+    """
+        Add new book with all its authors and genres
+
+        Return new book
+    """
     if request is None:
-        abort(400)
+        abort(make_response(WebError(error_code=400, msg="Request data error").dict(), 400))
     try:
-        book = BooksSchema(**request.form)
+        book = BookSchema(**request.json)
     except ValidationError as e:
         return "Exception" + e.json()
     _book = Books(**book.dict())
     db.add(_book)
+    book = Book_content.from_orm(book)
 
     # adding genres to the created book
-    genres_list_id = request.form["genres_id"].split(',')
-    genre_list = [db.query(Genres).get(gnr_id) for gnr_id in genres_list_id]
+    genre_list = [db.query(Genres).get(gnr_id)
+                  for gnr_id in request.json["genres_id"]
+                  if db.query(Genres).get(gnr_id) is not None]
+    if not genre_list:
+        abort(make_response(WebError(error_code=404, msg="Genres not found").dict(), 404))
     [gnr.book.append(_book) for gnr in genre_list]
 
     # adding authors to the created book
-    authors_list_id = request.form["authors_id"].split(',')
-    author_list = [db.query(Authors).get(author_id) for author_id in authors_list_id]
+    author_list = [db.query(Authors).get(author_id)
+                   for author_id in request.json["authors_id"]
+                   if db.query(Authors).get(author_id) is not None]
+    if not author_list:
+        abort(make_response(WebError(error_code=404, msg="Authors not found").dict(), 404))
     [author.book.append(_book) for author in author_list]
 
     db.commit()
     db.refresh(_book)
-    return jsonify(_book)
+    book.genres = genre_list
+    book.authors = author_list
+    return book
 
 
 @books_blueprint.put("/api/books/<int:book_id>")
+@spec.validate(json=Book_content_id,
+               resp=Response(HTTP_400=(WebError, "Request data error"),
+                             HTTP_404=(WebError, "Books not found"),
+                             HTTP_200=(Book_content, 'Successful operation')), tags=["Books_request"])
 def update_book(book_id, db: Session = Session()):
+    """
+        Update book with all its authors and genres
+
+        Return updated book
+    """
     if request is None:
-        abort(400)
+        abort(make_response(WebError(error_code=400, msg="Request data error").dict(), 400))
     try:
-        BooksSchema(**request.form)
+        BookSchema(**request.json)
     except ValidationError as e:
         return "Exception" + e.json()
     book = db.query(Books).get(book_id)
     if book is None:
-        abort(404)
-    book.book_name = request.form['book_name']
-    book.number_of_pages = request.form['number_of_pages']
-    book.rating = request.form['rating']
+        abort(make_response(WebError(error_code=404, msg="Book not found").dict(), 404))
+    book.book_name = request.json['book_name']
+    book.number_of_pages = request.json['number_of_pages']
+    book.rating = request.json['rating']
     db.add(book)
+    _book = Book_content.from_orm(book)
 
     [gnr.book.remove(book) for gnr in book.Books_Genres]
-    genres_list_id = request.form["genres_id"].split(',')
-    genre_list = [db.query(Genres).get(gnr_id) for gnr_id in genres_list_id]
+    genre_list = [db.query(Genres).get(gnr_id)
+                  for gnr_id in request.json["genres_id"]
+                  if db.query(Genres).get(gnr_id) is not None]
+    if not genre_list:
+        abort(make_response(WebError(error_code=404, msg="Genres not found").dict(), 404))
     [gnr.book.append(book) for gnr in genre_list]
 
     [author.book.remove(book) for author in book.Books_Authors]
-    authors_list_id = request.form["authors_id"].split(',')
-    author_list = [db.query(Authors).get(author_id) for author_id in authors_list_id]
+    author_list = [db.query(Authors).get(author_id)
+                   for author_id in request.json["authors_id"]
+                   if db.query(Authors).get(author_id) is not None]
+    if not author_list:
+        abort(make_response(WebError(error_code=404, msg="Authors not found").dict(), 404))
     [author.book.append(book) for author in author_list]
 
     db.commit()
     db.refresh(book)
-    return jsonify(book)
+    _book.genres = genre_list
+    _book.authors = author_list
+    return _book
 
 
 @books_blueprint.delete("/api/books/<int:book_id>")
+@spec.validate(resp=Response(HTTP_404=(WebError, "Books not found"),
+                             HTTP_200=(WebError, 'Successful operation')), tags=["Books_request"])
 def delete_book(book_id, db: Session = Session()):
+    """
+        Delete book
+
+        Return delete message
+    """
     book = db.query(Books).get(book_id)
     if book is None:
-        abort(404)
+        abort(make_response(WebError(error_code=404, msg="Books not found").dict(), 404))
     db.delete(book)
     db.commit()
-    return jsonify(True)
+    abort(make_response(WebError(error_code=200, msg="Book deleted").dict(), 200))
+    pass
